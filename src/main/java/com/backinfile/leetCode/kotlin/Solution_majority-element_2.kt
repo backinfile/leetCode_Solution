cf1e3a0517c986471bc5947074d686037a7f14ca
backinfile <1757518497@qq.com>
2023-03-08 10:39:49 +0800

    update

--- src/main/java/com/backinfile/leetCode/kotlin/Solution_majority-element_2.kt
+++ src/main/java/com/backinfile/leetCode/kotlin/Solution_majority-element_2.kt
@@ -1,19 +1,23 @@
 package com.backinfile.leetCode.kotlin
 
+import com.backinfile.Utils
 import com.backinfile.toIntArray
 import org.junit.Test
 import kotlin.random.Random
 
 class `Solution_majority-element_2` {
 
-    // 随机
+    // 不同的元素两两抵消，剩下的就是最多的（超过一半的）
     fun majorityElement(nums: IntArray): Int {
-        (0..Int.MAX_VALUE).asSequence()
-            .map { nums[Random.nextInt(nums.size)] }
-            .map { num -> num to nums.count { it == num } }
-            .filter { it.second > nums.size / 2 }
-            .forEach { return it.first }
-        return nums[0]
+        var result: Int = nums[0]
+        var count = 0
+        for (n in nums) {
+            if (count == 0) {
+                result = n
+            }
+            count += if (result == n) 1 else -1
+        }
+        return result
     }
 
 
@@ -21,5 +25,6 @@ class `Solution_majority-element_2` {
     fun test() {
         assert(3 == majorityElement("[3,2,3]".toIntArray()))
         assert(2 == majorityElement("[2,2,1,1,1,2,2]".toIntArray()))
+        assert(2333 == majorityElement(Utils.readResource("input_majority-element_01.txt").toIntArray()))
     }
 }
