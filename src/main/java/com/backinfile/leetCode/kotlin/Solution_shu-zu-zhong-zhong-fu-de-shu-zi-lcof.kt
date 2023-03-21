fd38aeecc24e3ce89ffa42da3b2c8fc35f2ac820
backinfile <1757518497@qq.com>
2023-03-21 18:06:57 +0800

    update

---
+++ src/main/java/com/backinfile/leetCode/kotlin/Solution_shu-zu-zhong-zhong-fu-de-shu-zi-lcof.kt
@@ -0,0 +1,30 @@
+package com.backinfile.leetCode.kotlin
+
+import com.backinfile.assertEqualTo
+import com.backinfile.toIntArray
+import org.junit.Test
+
+class `Solution_shu-zu-zhong-zhong-fu-de-shu-zi-lcof` {
+    fun findRepeatNumber(nums: IntArray): Int {
+        for (i in nums.indices) {
+            while (i != nums[i]) {
+                val curValue = nums[i]
+                if (nums[curValue] == curValue) {
+                    return curValue
+                }
+                nums.swap(i, curValue)
+            }
+        }
+        return -1
+    }
+
+    private fun IntArray.swap(a: Int, b: Int) {
+        if (a != b) this[a] = this[b].also { this[b] = this[a] }
+    }
+
+    @Test
+    fun test() {
+        1 assertEqualTo findRepeatNumber("[3,4,2,1,1,0]".toIntArray())
+        assert(findRepeatNumber("[2, 3, 1, 0, 2, 5, 3]".toIntArray()) in intArrayOf(2, 3))
+    }
+}
---
+++ src/main/java/com/backinfile/leetCode/kotlin/Solution_shu-zu-zhong-zhong-fu-de-shu-zi-lcof_2.kt
@@ -0,0 +1,25 @@
+package com.backinfile.leetCode.kotlin
+
+import com.backinfile.assertEqualTo
+import com.backinfile.toIntArray
+import org.junit.Test
+
+class `Solution_shu-zu-zhong-zhong-fu-de-shu-zi-lcof_2` {
+    fun findRepeatNumber(nums: IntArray): Int {
+        val size = nums.size
+        for (i in nums.indices) {
+            val curValue = if (nums[i] < 0) nums[i] + size else nums[i]
+            if (nums[curValue] < 0) {
+                return curValue
+            }
+            nums[curValue] -= size
+        }
+        return -1
+    }
+
+    @Test
+    fun test() {
+        1 assertEqualTo findRepeatNumber("[3,4,2,1,1,0]".toIntArray())
+        assert(findRepeatNumber("[2, 3, 1, 0, 2, 5, 3]".toIntArray()) in intArrayOf(2, 3))
+    }
+}
